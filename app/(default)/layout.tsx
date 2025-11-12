b3b52fabd87b4bca12142bfc1ec368825889ba88
russuiulia <russuiulia.md@gmail.com>
2025-11-12 15:34:48 +0200

    ogimage

--- app/(default)/layout.tsx
+++ app/(default)/layout.tsx
@@ -24,7 +24,16 @@ export default function DefaultLayout({
   return (
     <>
       <main className="relative flex grow flex-col">{children}</main>
-
+      <meta name="description" content="Fullstack developer, projects, and work experience." />
+      <meta property="og:title" content="My Portfolio - Russu Iulia" />
+      <meta property="og:description" content="Fullstack developer, projects, and work experience." />
+      <meta property="og:image" content="https://russuiulia.vercel.app/ogimage.png" />
+      <meta property="og:url" content="https://russuiulia.vercel.app/" />
+      <meta property="og:type" content="website" />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content="My Portfolio - Russu Iulia" />
+      <meta name="twitter:description" content="Fullstack developer, projects, and work experience." />
+      <meta name="twitter:image" content="https://russuiulia.vercel.app/ogimage.png" />
       <Footer />
     </>
   );
